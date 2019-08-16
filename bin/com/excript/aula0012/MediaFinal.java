2fa5156362247a7e676d84a49d52aef80e56e279
Mateus Santos <mateus.h.santos@outlook.com.br>
2019-08-15 23:50:44 -0300

    Update MediaFinal.java

--- bin/com/excript/aula0012/MediaFinal.java
+++ bin/com/excript/aula0012/MediaFinal.java
@@ -3,24 +3,18 @@ package com.excript.aula0012;
 import java.util.Scanner;
 
 public class MediaFinal {
-
 	public static void main (String[]args) {
 		
 		Scanner in = new Scanner(System.in);		
 		double nota1, nota2, nota3, nota4, mediaminima;
-		
 		System.out.println("Digite sua média mínima:");
 		mediaminima = in.nextDouble();
-		
 		System.out.println("Digite sua nota:");
 		nota1 = in.nextDouble();
-		
 		System.out.println("Digite sua segunda nota:");
 		nota2 = in.nextDouble();
-		
 		System.out.println("Digite sua terceira nota:");
 		nota3 = in.nextDouble();
-		
 		System.out.println("Digite sua quarta nota:");
 		nota4 = in.nextDouble();
 		
